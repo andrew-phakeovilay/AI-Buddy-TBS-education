648c6979f72abc14b7ac3492b1c54f4eda4f9d8e
Andrew <95313598+andrew-phakeovilay@users.noreply.github.com>
2025-11-26 15:46:37 +0100

    login tailwind

--- ia-buddy-front-end/src/pages/Login.tsx
+++ ia-buddy-front-end/src/pages/Login.tsx
@@ -1,31 +1,55 @@
 import { useTranslation } from "react-i18next";
-import "../style/login.css"
 
 export function Login() {
-  
+
   const { t } = useTranslation();
-  
+
   return (
-    <div className="login-container">
-      <form className="login-form">
-        <h1 className="login-title">AI Buddy</h1>
-        <p className="login-description">{t("login-description-form")}</p>
+    <div className="min-h-screen bg-white flex justify-center items-center">
+      <form className="w-full max-w-sm text-center text-black">
+        
+        <h1 className="text-2xl font-bold mb-1">AI Buddy</h1>
+        <p className="text-sm text-gray-400 mb-10">
+          {t("login-description-form")}
+        </p>
 
-        <ul>
-          <li>
-            <label htmlFor="id">{t("login-id-label-form")}</label>
-            <input placeholder={t("login-id-placeholder-form")} type="id" />
+        <ul className="space-y-6">
+          <li className="flex flex-col text-left">
+            <label htmlFor="id" className="mb-1 text-sm">
+              {t("login-id-label-form")}
+            </label>
+
+            <input
+              placeholder={t("login-id-placeholder-form")}
+              type="text"
+              id="id"
+              className="bg-[#d1d9e6] text-black rounded-md px-3 py-3 text-sm focus:outline-none"
+            />
           </li>
 
-          <li>
-            <label htmlFor="password">{t("login-password-label-form")}</label>
-            <input placeholder={t("login-password-placeholder-form")} type="password" />
+          <li className="flex flex-col text-left">
+            <label htmlFor="password" className="mb-1 text-sm">
+              {t("login-password-label-form")}
+            </label>
+
+            <input
+              placeholder={t("login-password-placeholder-form")}
+              type="password"
+              id="password"
+              className="bg-[#d1d9e6] text-black rounded-md px-3 py-3 text-sm focus:outline-none"
+            />
           </li>
 
-          <li>
-            <button type="submit">{t("login-submit-form")}</button>
+          <li className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-rose-600 hover:bg-rose-400 text-white px-6 py-3 text-base rounded-md cursor-pointer transition w-1/2"
+            >
+              {t("login-submit-form")}
+            </button>
           </li>
         </ul>
+
       </form>
     </div>
   );
