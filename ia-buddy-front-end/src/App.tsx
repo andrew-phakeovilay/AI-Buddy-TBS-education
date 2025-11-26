d435b058cb07662877e217a901a16b6e86d68176
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-26 12:03:55 +0100

    Ajout de custom theme - Red, Bleu, Mint, Sage

--- ia-buddy-front-end/src/App.tsx
+++ ia-buddy-front-end/src/App.tsx
@@ -6,29 +6,29 @@ import { Admin } from './pages/Admin'
 import { ChatPage } from './pages/ChatPage'
 
 function App() {
-  
+
 
   return (
     <BrowserRouter>
-      <div className='flew flex-col min-h-screen h-screen '>
+      <div className='flew flex-col min-h-screen h-screen overflow-hidden'>
         {/* <nav className='absolute right-0 bg-red-12 flex justify-center gap-3'> */}
 
-      <nav className="flex justify-center items-center gap-6 p-4">
+        <nav className="flex justify-center items-center gap-6 p-4">
 
 
-        <Link to="/login">Connexion à l'outil</Link>
-        <Link to="/admin">Administration</Link>
-        <Link to="/chat/1">Chat 1</Link>
-        <button className="bg-rose-600 px-3 py-1 rounded-md text-sm text-white">FR / EN</button>
-      </nav>
+          <Link to="/login">Connexion à l'outil</Link>
+          <Link to="/admin">Administration</Link>
+          <Link to="/chat/1">Chat 1</Link>
+          <button className="bg-rose-600 px-3 py-1 rounded-md text-sm text-white">FR / EN</button>
+        </nav>
 
-      <Routes>
+        <Routes>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
 
-        <Route path='/chat/:id' element={<ChatPage />} />
-      </Routes>
+          <Route path='/chat/:id' element={<ChatPage />} />
+        </Routes>
 
 
 
