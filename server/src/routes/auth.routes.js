22dfc636d5b36145e184a889a83e2d4d6e99739b
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-12-17 16:19:29 +0100

    déplacement de auth.routes.js utilise fakeDB.fakeUser

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,20 +1,18 @@
 import { Router } from "express";
+import { fakeDB } from "../db/fakeDb.js";
 
 const router = Router();
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-
+  const fakeUser = fakeDB.fakeUser
   // Vérifier que les champs existent
   if (!username || !password) {
     return res.status(400).json({ error: "Email et mot de passe requis" });
   }
 
   // Exemples de valeurs test (à remplacer par ta base de données plus tard)
-  const fakeUser = {
-    username: "admin",
-    password: "1234",
-  };
+  
 
   if (username === fakeUser.username && password === fakeUser.password) {
     return res.json({ message: "Connexion réussie !", username });
